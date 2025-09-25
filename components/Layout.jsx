8e6e40304131ddd844db2e0eff5c93503f7fa185
David Litton <littond@msoe.edu>
2025-09-25 13:36:45 -0500

    feat: added layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,9 @@
+export default Layout = () => {
+  return (
+    <div>
+      {/* where the header will go */}
+      {/* where the main content will go */}
+      {/* where the footer will go */}
+    </div>
+  );
+};
