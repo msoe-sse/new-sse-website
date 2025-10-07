cfc16f171a8cfea11f2eeabfa2dab02e6e37bbd6
Andy Dao <daoa@msoe.edu>
2025-10-07 13:36:44 -0500

    DayNightSwitch in LayOut

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,11 @@
+import DayNightSwitch from "@components/DayNightSwitch.jsx";
 import "./Layout.css"
 
 export default function Layout() {
   return (
     <div>
       {/* where the header will go */}
+        <DayNightSwitch label={"Day/Night Mode"} />
       {/* where the main content will go */}
       {/* where the footer will go */}
     </div>
